fe7a73545da19ee520f94c99d88c04462d09b33b
whiskywawii <mikheylajoie@gmail.com>
2026-05-09 16:23:07 +0800

    Comment out Firebase module imports
    Comment out Firebase imports for future use.

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,6 @@
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
-import { getAuth } from 'firebase/auth';
+//import { initializeApp } from 'firebase/app';
+//import { getFirestore } from 'firebase/firestore';
+//import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
   apiKey: "AIzaSyCuDx2u6ismS9sDuXHCG8RVtYQUyxtVMdU",
